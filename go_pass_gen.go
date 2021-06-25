a26fdcbf193c9b9236530ffc4622cc2766cb5407
ryoma okuda <catontheskate4423@gmail.com>
2021-06-25 21:20:30 +0900

    スケルトンの追加

---
+++ go_pass_gen.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// TODO: OD
+	// welcome文を作る
+
+	// パスワードの長さを受け取る: int
+	fmt.Println("パスワードの文字数を入力してください(１２文字以下):")
+
+	// 誕生日の4桁数字を受け取る: int
+
+	// passwordMaker(文字数, 誕生日)を呼び出してパスワードを生成し、表示する。
+}
+
+func passwordMaker(pass_length int, birth int) string {
+	// TODO: RYOMA
+	// パスワードを生成する！
+	return "PASSWORD"
+}
